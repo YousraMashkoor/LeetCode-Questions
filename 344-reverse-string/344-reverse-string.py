0694ffa1a55bf558aa5744af5a468ff6aab77ce2
Yousra Mashkoor <yousra.mashkoor@gmail.com>
2022-04-01 10:02:20 -0900

    Time: 222 ms (73.92%), Space: 19.2 MB (8.51%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -4,12 +4,14 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         
+       #*************** Simple Pythonic way
+        
         # s[:] = s[::-1]
+        
+        
+        # ************* Problem solving approah
         
         x = []
         for i in range(len(s)-1, -1, -1):
             x.append(s[i])
-            
-        print(x)
-            
         s[:] = x[:]
