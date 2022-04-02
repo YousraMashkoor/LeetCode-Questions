14cfe5bd06244f4942fa97f98c81d17b44c95167
Yousra Mashkoor <yousra.mashkoor@gmail.com>
2022-04-02 09:55:05 -0900

    Time: 52 ms (99.97%), Space: 14.6 MB (52.23%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -15,33 +15,12 @@ class Solution:
             else:
                 return False
             
-        def choseIndex(start, end):
-            """
-            Choose which index to remove to form a plindrome
-            """
-            if s[start+1] == s[end]:
-                return start
-            else:
-                return end
-            
-            
             
         if isPalindrome(s):
             return True
         else:
             i1, i2 = diffChar(s,s[::-1])
-            # print(s[i1], s[:i1]+s[i1+1:])
             if isPalindrome(s[:i1]+s[i1+1:]):
                 return True
             else:
                 return isPalindrome(s[:i2]+s[i2+1:])
-            
-            
-            
-            # print(s[i1],s[i2])
-            # i = choseIndex(i1,i2)
-            # print(s[i])
-            # s = s[:i]+s[i+1:]
-            # return isPalindrome(s)
-            
-            
